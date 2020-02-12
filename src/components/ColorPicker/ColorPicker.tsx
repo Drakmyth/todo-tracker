bc5a0c675fe5ecdcb03998d543287cbce3bc8783
Drakmyth <shaun.hamman@gmail.com>
2020-02-12 08:03:56 -0800

    Fix ColorPicker cancel handling. Still working on styling...

--- src/components/ColorPicker/ColorPicker.tsx
+++ src/components/ColorPicker/ColorPicker.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, MouseEvent } from 'react';
 import './ColorPicker.scss';
 import { SketchPicker } from 'react-color';
-import Modal from '../Modal/Modal';
 import ReactDOM from 'react-dom';
 
 interface ColorPickerProps {
@@ -14,6 +13,8 @@ const ColorPicker: React.FC<ColorPickerProps> = (props) => {
     const [color, setColor] = useState(props.color)
     const [previousColor, setPreviousColor] = useState(props.color)
 
+    const popover_ref = React.createRef<HTMLDivElement>();
+
     useEffect(() => {
         setPreviousColor(props.color);
         setColor(props.color);
@@ -23,30 +24,39 @@ const ColorPicker: React.FC<ColorPickerProps> = (props) => {
         setDisplayColorPicker(true);
     }
 
-    const pickColor = () => {
+    const pickColor = (event: MouseEvent<HTMLElement>) => {
         setDisplayColorPicker(false);
         props.onChange(color)
     }
-
-    const cancelColor = () => {
+    
+    const cancelColor = (event: MouseEvent<HTMLElement>) => {
         setDisplayColorPicker(false);
         setColor(previousColor);
+    }
+    
+    const cancelClickAway = (event: MouseEvent<HTMLElement>) => {
+        if (event.target !== popover_ref.current) {
+            event.stopPropagation();
+            return;
+        }
+        cancelColor(event);
     }
 
     return <div className="ColorPicker">
         <div className="cp-preview" onClick={openPicker} style={{ background: color }}></div>
         <span className="cp-hex">{color}</span>
         {displayColorPicker &&
-            // <Modal show={displayColorPicker}
-            //     onClose={pickColor}
-            //     onCancel={cancelColor}
-            //     className="ColorPickerModal"
-            //     cover={false}>
-            ReactDOM.createPortal(<div className="popover">
-                <div className="cover" onClick={pickColor}/>
-                <SketchPicker  color={color} onChange={(c) => setColor(c.hex)} />
-            </div>, document.body)
-            // </Modal>
+            ReactDOM.createPortal(
+                <div className="popover" ref={popover_ref} onClick={cancelClickAway}>
+                    <div className="color-picker-modal">
+                        <SketchPicker color={color} onChange={(c) => setColor(c.hex)} />
+                        <div className="color-picker-buttons">
+                            <button onClick={pickColor}>Apply</button>
+                            <button onClick={cancelColor}>Cancel</button>
+                        </div>
+                    </div>
+                </div>, document.body
+            )
         }
     </div>
 }
